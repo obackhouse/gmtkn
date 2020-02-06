30f109bead083c2862b7d29fbd439c856a30d421
Ollie Backhouse <olbackhouse@gmail.com>
2020-02-06 09:37:55 +0000

    Adds list of modules to the python file

--- gmtkn.py
+++ gmtkn.py
@@ -52,3 +52,59 @@ from sets import W4_11, \
                  SCONF, \
                  UPU23, \
                  BUT14DIOL
+
+sets = [ W4_11, \
+         G21EA, \
+         G21IP, \
+         DIPCS10, \
+         PA26, \
+         SIE4x4, \
+         ALKBDE10, \
+         YBDE18, \
+         AL2X6, \
+         HEAVYSB11, \
+         NBPRC, \
+         ALK8, \
+         RC21, \
+         G2RC, \
+         FH51, \
+         TAUT15, \
+         DC13, \
+         MB16_43, \
+         DARC, \
+         RSE43, \
+         BSR36, \
+         CDIE20, \
+         ISO34, \
+         ISOL24, \
+         C60ISO, \
+         PArel, \
+         BH76, \
+         BHPERI, \
+         BHDIV10, \
+         INV24, \
+         BHROT27, \
+         PX13, \
+         WCPT18, \
+         RG18, \
+         ADIM6, \
+         S22, \
+         S66, \
+         HEAVY28, \
+         WATER27, \
+         CARBHB12, \
+         PNICO23, \
+         HAL59, \
+         AHB21, \
+         CHB6, \
+         IL16, \
+         IDISP, \
+         ICONF, \
+         ACONF, \
+         Amino20x4, \
+         PCONF21, \
+         MCONF, \
+         SCONF, \
+         UPU23, \
+         BUT14DIOL
+]
