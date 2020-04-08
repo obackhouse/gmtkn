676a0a069c7fbfe5d0404c709cdd773552967d32
Ollie Backhouse <olbackhouse@gmail.com>
2020-04-08 08:58:16 +0100

    Fixes some imports (still might need tweaking if anyone actually wants to use this repo)

--- gmtkn.py
+++ gmtkn.py
@@ -53,58 +53,58 @@ from sets import W4_11, \
                  UPU23, \
                  BUT14DIOL
 
-sets = [ W4_11, \
-         G21EA, \
-         G21IP, \
-         DIPCS10, \
-         PA26, \
-         SIE4x4, \
-         ALKBDE10, \
-         YBDE18, \
-         AL2X6, \
-         HEAVYSB11, \
-         NBPRC, \
-         ALK8, \
-         RC21, \
-         G2RC, \
-         FH51, \
-         TAUT15, \
-         DC13, \
-         MB16_43, \
-         DARC, \
-         RSE43, \
-         BSR36, \
-         CDIE20, \
-         ISO34, \
-         ISOL24, \
-         C60ISO, \
-         PArel, \
-         BH76, \
-         BHPERI, \
-         BHDIV10, \
-         INV24, \
-         BHROT27, \
-         PX13, \
-         WCPT18, \
-         RG18, \
-         ADIM6, \
-         S22, \
-         S66, \
-         HEAVY28, \
-         WATER27, \
-         CARBHB12, \
-         PNICO23, \
-         HAL59, \
-         AHB21, \
-         CHB6, \
-         IL16, \
-         IDISP, \
-         ICONF, \
-         ACONF, \
-         Amino20x4, \
-         PCONF21, \
-         MCONF, \
-         SCONF, \
-         UPU23, \
-         BUT14DIOL
-]
+sets = { 'W4_11'     : W4_11, \
+         'G21EA'     : G21EA, \
+         'G21IP'     : G21IP, \
+         'DIPCS10'   : DIPCS10, \
+         'PA26'      : PA26, \
+         'SIE4x4'    : SIE4x4, \
+         'ALKBDE10'  : ALKBDE10, \
+         'YBDE18'    : YBDE18, \
+         'AL2X6'     : AL2X6, \
+         'HEAVYSB11' : HEAVYSB11, \
+         'NBPRC'     : NBPRC, \
+         'ALK8'      : ALK8, \
+         'RC21'      : RC21, \
+         'G2RC'      : G2RC, \
+         'FH51'      : FH51, \
+         'TAUT15'    : TAUT15, \
+         'DC13'      : DC13, \
+         'MB16_43'   : MB16_43, \
+         'DARC'      : DARC, \
+         'RSE43'     : RSE43, \
+         'BSR36'     : BSR36, \
+         'CDIE20'    : CDIE20, \
+         'ISO34'     : ISO34, \
+         'ISOL24'    : ISOL24, \
+         'C60ISO'    : C60ISO, \
+         'PArel'     : PArel, \
+         'BH76'      : BH76, \
+         'BHPERI'    : BHPERI, \
+         'BHDIV10'   : BHDIV10, \
+         'INV24'     : INV24, \
+         'BHROT27'   : BHROT27, \
+         'PX13'      : PX13, \
+         'WCPT18'    : WCPT18, \
+         'RG18'      : RG18, \
+         'ADIM6'     : ADIM6, \
+         'S22'       : S22, \
+         'S66'       : S66, \
+         'HEAVY28'   : HEAVY28, \
+         'WATER27'   : WATER27, \
+         'CARBHB12'  : CARBHB12, \
+         'PNICO23'   : PNICO23, \
+         'HAL59'     : HAL59, \
+         'AHB21'     : AHB21, \
+         'CHB6'      : CHB6, \
+         'IL16'      : IL16, \
+         'IDISP'     : IDISP, \
+         'ICONF'     : ICONF, \
+         'ACONF'     : ACONF, \
+         'Amino20x4' : Amino20x4, \
+         'PCONF21'   : PCONF21, \
+         'MCONF'     : MCONF, \
+         'SCONF'     : SCONF, \
+         'UPU23'     : UPU23, \
+         'BUT14DIOL' : BUT14DIOL
+}
